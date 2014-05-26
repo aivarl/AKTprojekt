318118537333b4d102ff048b8287179a646ff251
aivarl <aivarlobjakas@gmail.com>
2014-05-26 03:15:05 +0300

    .py

---
+++ TurtleEst.py
@@ -0,0 +1,115 @@
+from turtle import *
+
+#http://stackoverflow.com/questions/4998629/python-split-string-with-multiple-delimiters
+def split(delimiters, string, maxsplit=0):
+    import re
+    regexPattern = '|'.join(map(re.escape, delimiters))
+    return re.split(regexPattern, string, maxsplit)
+#shape('turtle')
+def korduste_töötlus(inputStr):
+    command_string = ''
+    delimiters = ":", "."
+    input_list = split(delimiters,inputStr)
+    #print(input_list)
+    i=0
+    while i<len(input_list):
+        for el in input_list[i].split():
+            #print(input_list[i])
+            #print(el)
+            if el.isdigit():
+                kordaja = int(el)
+        #print(kordaja)
+        string = input_list[i+1]
+        #print(string)
+        j = 0
+        while j<kordaja:
+            j += 1
+            command_string += string + ","
+        #command_string += string*kordaja + ","
+        #print(command_string)
+        i+=2
+    delimiters2 = ";", " ja ", ","
+    #commands = split(delimiters2, command_string)
+    return command_string
+## Käskluste realiseerimine
+def do(word, argument):
+    word=word.lower()
+    if word in liigu_edasi: #== "edasi" or word.lower() == "otse":
+        forward(int(argument))            
+    if word in liigu_tagasi: #== "tagasi" or word.lower() == "tagurpidi":
+        backward(int(argument))
+        #if x== "2 sammu edasi":
+    if word in paremale: #== "paremale" or word.lower() == "päripäeva":
+        right(int(argument))
+    if word.lower() in vasakule: #== "vasakule" or word.lower() == "vastupäeva":
+        left(int(argument))
+    if word in lõpeta: #== "lõpeta":
+        exitonclick()
+    if word == "kiirus":
+        speed(int(argument))
+    #pliiats üles ja alla paremad märksõnad?
+    if word == "üles":
+        up()
+    if word == "alla":
+        down()
+    if word == "suurus" or word=='paksus':
+        pensize(int(argument))
+    ## VÄRVID
+    if word in värvid:
+        pencolor(värvid.get(word))
+    ## Kilpkonna nähtavus
+    if word == "nähtav":
+        showturtle()
+    if word == "nähtamatu":
+        hideturtle()
+    ## Ilmakaared
+    if word in ilmakaared:
+        setheading(ilmakaared.get(word))
+
+## korduste_töötlus kaotab ära korduskäsklusele eelnevad käsud
+## executeCommands hakkab käske algusest täitma, kuni jõuab korduskäsuni
+def executeCommands(inputStr):
+    delimiters = ";", " ja ", ","
+    commandslist = split(delimiters, inputStr)
+    for command in commandslist:
+        shape('turtle')
+        words = command.split()
+        if ':' in command:
+            executeCommands(korduste_töötlus(inputStr))
+            return  ## funktsiooni töö võib peatada
+        for word in words:
+            if word.isdigit():
+                argument = word
+            else:
+                argument = 0
+        for word in words:
+            ## Eitused
+            if word in eitused:
+                break
+            else:
+                do(word, argument)            
+    argument = 0
+        
+
+
+liigu_edasi = ['edasi', 'otse', 'otsejoones', 'silmalt', 'silmmnäolt', 'otseteed', 'õkva']
+liigu_tagasi = ['tagasi', 'tagurpidi']
+paremale = ['paremale', 'päripäeva']
+vasakule = ['vasakule', 'vastupäeva']
+lõpeta = ['lõpeta', 'lõpetan', 'quit', 'exit', '']
+eitused = ['ära', 'mitte', 'ei']
+värvid = {'punane':'red', 'oranž':'orange', 'kollane':'yellow', 'roheline':'green' ,'sinine': 'blue'}
+ilmakaared = {'põhja': 90, 'lõunasse': 270, 'itta':0, 'läände': 180, 'kirdesse': 45, 'kagusse': 315, 'edelasse': 225, 'loodesse': 135}
+
+while True:
+    x=input("Anna käsklus: ")
+    #if x.split(':') != [x]:
+        #x=korduste_töötlus(x)
+    #delimiters = ";", " ja ", ","
+    #commands = split(delimiters, x)
+    #commands=x.split(";")
+    #print('-20'.isdigit())
+    #print(commands)
+    executeCommands(x)
+
+#exitonclick()
